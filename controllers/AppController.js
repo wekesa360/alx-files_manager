bd31816366be2f13af56b5fcc7f8267a9264fd2c
Michael Wekesa <wekesa360@outlook.com>
2022-06-15 12:48:19 +0300

    add app controller

---
+++ controllers/AppController.js
@@ -0,0 +1,20 @@
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+class AppController {
+    static getStatus(req, res) {
+        const alive = {
+            redis: redisClient.isAlive(),
+            db: dbClient.isAlive(),
+        };
+        return res.status(200).send(alive);
+    }
+
+    static async getStats(req, res) {
+        const stats = {
+            users: await dbClient.nbUsers(),
+            files: await dbClient.nbFiles(),
+        };
+        return res.status(200).send(stats);
+    }
+}
